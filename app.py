6c3638560d4264003cc5c80ecb07fd5deb22b6a8
pawoods <>
2023-10-31 22:00:27 +0000

    adds functionality to delete recipe to app.py

--- app.py
+++ app.py
@@ -193,8 +193,11 @@ def edit_recipe():
     return render_template("edit_recipe.html")
 
 
-@app.route("/delete_recipe")
-def delete_recipe():
+@app.route("/delete_recipe/<recipe_id>")
+def delete_recipe(recipe_id):
+    mongo.db.recipes.delete_one({"_id": ObjectId(recipe_id)})
+
+    flash("Recipe successfully deleted")
     return redirect(url_for("recipes"))
 
 
