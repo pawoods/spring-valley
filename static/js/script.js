1176a6c3a9b1177148e578f11375d02fe3c900de
pawoods <>
2023-10-31 20:54:00 +0000

    adds javascript functions to add and remove fields in add_recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function () {
     $('.sidenav').sidenav({edge: "right"});
 });
+
+function add(event) {
+    console.log(event.target);
+    let element = event.target.parentNode;
+    let newElement = element.cloneNode(true);
+    newElement.querySelector("input").value = "";
+    element.insertAdjacentElement('afterend', newElement);
+}
+
+function remove(event) {
+    console.log(event.target);
+    let element = event.target.parentNode;
+    element.remove();
+}
